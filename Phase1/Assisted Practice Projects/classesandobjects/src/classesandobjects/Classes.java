913e9dd4c052b2b0a7fec6dc9205dca7b6eb3b69
Shahira12345 <97025460+Shahira12345@users.noreply.github.com>
2022-01-28 11:56:35 +0530

    Initial Commit

---
+++ Phase1/Assisted Practice Projects/classesandobjects/src/classesandobjects/Classes.java
@@ -0,0 +1,19 @@
+package classesandobjects;
+
+public class Classes {
+   
+    int id=12;
+    double salary=2000;
+    String gender="male";
+    String name="ravi";
+    public static void main(String[] args) {
+   	 
+   	 Classes person=new Classes();
+    
+   	 System.out.println(person.id);
+   	 System.out.println(person.name);
+   	 
+    }
+
+}
+
